85f9325d6ade45868c2af79ac940d11c99177b82
Fyrlex <fyrlex@gmail.com>
2022-10-25 12:54:19 -0400

    remove unused props

--- src/managers/ProjectManager.ts
+++ src/managers/ProjectManager.ts
@@ -1,16 +1,11 @@
-import { Collection } from '@discordjs/collection';
 import { Project } from 'luyx-management-api-types/v1';
 import { LuyxClient } from '../client/LuyxClient.js';
 import { LuyxProject } from '../structures/Project.js';
 import { CachedManager } from './CachedManager.js';
 
 export class ProjectManager extends CachedManager<'projects'> {
-	public readonly cache: Collection<string, Project>;
-
 	constructor(client: LuyxClient) {
 		super('projects', client);
-
-		this.cache = new Collection();
 	}
 
 	protected resolve(data: Project): Project {
