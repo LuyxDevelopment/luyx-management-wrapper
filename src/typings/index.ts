3f078094fc41ac59e04aa9304627a57b6bf9aefe
Fyrlex <fyrlex@gmail.com>
2022-10-29 15:21:16 -0400

    remove unused types

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,13 +1,7 @@
-import { PostProjectRouteOptions, PostTransactionRouteOptions, PostUserRouteOptions, Project, Transaction, User } from 'luyx-management-api-types/v1';
+import { Project, Transaction, User } from 'luyx-management-api-types/v1';
 import { LuyxProject } from '../structures/Project.js';
 import { LuyxTransaction } from '../structures/Transaction.js';
 import { LuyxUser } from '../structures/User.js';
-
-export interface DataBuild {
-	projects: PostProjectRouteOptions['Body'];
-	transactions: PostTransactionRouteOptions['Body'];
-	users: PostUserRouteOptions['Body'];
-}
 
 export interface DataInterface {
 	projects: Project;
@@ -20,9 +14,3 @@ export interface DataStructure {
 	transactions: LuyxTransaction;
 	users: LuyxUser;
 }
-
-export type DeepPartial<T> = T extends object
-	? {
-		[P in keyof T]?: DeepPartial<T[P]>;
-	}
-	: T;
